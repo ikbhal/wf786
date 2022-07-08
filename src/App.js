bf33019cccc96ba0c93a8f822938a4c68f422aa1
Shaik Ikbhal Basha <iqbalforall@gmail.com>
2022-07-08 20:00:36 +0530

    path, zoom in not wokring yet, add sibling next tried

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import './App.css';
 let nodes = [];
 let nodeIdLast = 4;
 let startNode = null;
+let zoomNode = null;
+let pathNodes = [];
 
 
 let n2 = {id: 2, text: "n2", closed: true, children: []};
@@ -19,6 +21,11 @@ nodes.push(n4);
 
 //console.log("nodes:",nodes);
 startNode= n1;
+//set zoom node 
+zoomNode = startNode;
+//add startnode to path nodes;
+pathNodes.push(startNode);
+
 //console.log("startNode: ", startNode);
 
 function App() {
@@ -29,12 +36,58 @@ function App() {
   //   setText(e.target.value);
     
   // };
+  const [getZoomNode, setZoomNode] = useState(zoomNode);
   return (
     <div className="App">
       <h1>Workflowy</h1>
+      <PathSection/>
       {/* <input type="text" value={text} onChange={onChangeHandler}/> */}
-     <Node node={startNode}/>
+     <Node node={getZoomNode} 
+      parentNode={null}
+      setZoomNode={setZoomNode}/>
     </div>
+  );
+}
+
+function PathSection(){
+  console.log("inside pathsection pathNodes:", pathNodes);
+
+  const pathPartHandler = (node) =>{
+    console.log("inside path part handler, node: ", node);
+  };
+
+  pathNodes.map( (node, index) => {
+    console.log("path nodess map node:", node);
+  });
+  var pathNodeComps = pathNodes.map( (node, index) => 
+  <PathPart 
+    node={node} 
+    key={index}
+    onClick={e=> pathPartHandler(node)}
+    >
+
+    </PathPart>
+  );
+  return (
+    <>
+      <h2>Path Section</h2>
+      <div className="path_section">
+        {pathNodeComps}
+      </div>
+    </>
+  );
+}
+
+function PathPart({node, onClick}){
+  console.log("inside path part node:", node ,
+  "onClick:", onClick);
+  return (
+    <>
+    <span className="path-part">
+      {node.text}  and 
+    </span>
+    {/* <span>|</span> */}
+    </>
   );
 }
 
@@ -45,7 +98,7 @@ function useForceUpdate(){
   // is better than directly setting `value + 1`
 }
 
-function Node({node}) {
+function Node({node, parentNode, setZoomNode}) {
   // console.log("inside node  data ...", node);
   var {id, text, closed, children} = node; 
   // console.log("inside node id:", id, ", text:",
@@ -91,11 +144,51 @@ function Node({node}) {
     setNodeText(e.target.value);
   };
 
+  const addNextSiblingNode = (node, parentNode) => {
+    console.log("inside addNextSiblingNode  node:",
+      node , ", parentNode:", parentNode);
+    if(parentNode != null){
+      let  parentNodeChildren = parentNode.children;
+      if(parentNodeChildren != null){
+        let nodeIndex = parentNodeChildren.indexOf(node);
+        // create empty node
+        // increment node id  last counter
+        nodeIdLast++;
+        let newChildNode = {
+          id: nodeIdLast, 
+          text: "n"+nodeIdLast,
+          closed: true, 
+          children: []
+        };
+        // insert new child node at  next postion of current node at parent node children array
+        parentNodeChildren.splice(nodeIndex+1, 0, newChildNode);
+        // rerender trigger -> not working
+        //forceUpdate();
+      }
+    }
+  };
+
+  const setZoomNodeHandler = (node) =>{
+    console.log("inside zoom node: ", node);
+    if(startNode != node){
+      zoomNode = node;
+      setZoomNode(zoomNode);
+      //add current node to path Nodes;
+      pathNodes.push(zoomNode);
+    }
+  };
+
   const forceUpdate = useForceUpdate();
 
   var childNodes = null;
   if(!isClosed){
-    childNodes = getChildren.map((child, index) => <Node key={index} node={child}/>);
+    childNodes = getChildren.map((child, index) => 
+      <Node 
+        key={index} 
+        node={child} 
+        parentNode={node}
+        setZoomNode={setZoomNode} />
+    );
   }
   return (
     <div className="node">
@@ -105,12 +198,28 @@ function Node({node}) {
      </span>
      <span className="add-child-node" 
       onClick={e=>addChildNode(node, setChildren)} >
-        +c
+        add child
      </span>
+     {parentNode !=null && 
      <span className="delete-node" 
       onClick={e=>deleteChildNode(node)} >
-        x
+        delete
      </span>
+     }
+     {parentNode !=null && 
+     <span className="add-next-sibling-node"
+      onClick={e=>addNextSiblingNode(node, parentNode)}
+     >
+      add next sibling
+     </span>
+     }
+     {startNode !=node && 
+     <span className="zoom-node"
+      onClick={e=>setZoomNodeHandler(node)}
+     >
+      zoom in
+     </span>
+     }
 
      {!isNodeTextEdit && 
       <span className="node-text"
